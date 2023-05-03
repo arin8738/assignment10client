17506261b53b03be0cb1095b27056036e3a4350c
arin8738 <arinsikder06@gmail.com>
2023-05-03 18:25:17 +0600

    footer added

--- src/componends/header/banner.jsx
+++ src/componends/header/banner.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
-            <div>
-                <h1>ok11</h1>
+        <div className='bg-base-200'>
+            <div className='pt-6 pb-6 pl-8'>
+                <img className='w-1/2 rounded-xl  ' src="https://images.unsplash.com/photo-1600565193348-f74bd3c7ccdf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2hlZnxlbnwwfHwwfHw%3D&w=1000&q=80" alt="" />
+
             </div>
             <div>
 
             </div>
         </div>
-    );
+    ); 
 };
 
 export default Banner;
